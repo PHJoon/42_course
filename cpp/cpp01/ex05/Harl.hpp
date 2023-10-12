7293d0dab343124b691d0ad8ce44ddc7c2627ee3
Hyungjun Park <hyungjpa@c5r8s7.42seoul.kr>
2023-10-12 13:03:08 +0900

    cpp01-ex05

---
+++ cpp/cpp01/ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyungjpa <hyungjpa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 12:32:38 by hyungjpa          #+#    #+#             */
+/*   Updated: 2023/10/12 12:37:42 by hyungjpa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+    private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+    public:
+        Harl();
+        ~Harl();
+        void    complain(std::string level);
+};
+
+
+#endif
