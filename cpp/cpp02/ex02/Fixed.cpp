23fc1571e9b825f753b66e4f1c4c9ad1ec57bb18
PHJoon <phj9503@gmail.com>
2023-10-20 09:48:50 +0900

    cpp02-ex02

--- cpp/cpp02/ex02/Fixed.cpp
+++ cpp/cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hyungjpa <hyungjpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:18:52 by hyungjpa          #+#    #+#             */
-/*   Updated: 2023/10/20 09:19:46 by hyungjpa         ###   ########.fr       */
+/*   Updated: 2023/10/20 09:48:37 by hyungjpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,33 @@
 
 Fixed::Fixed(void): _fixed_point_num(0)
 {
-
+    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int fixed_point_num): _fixed_point_num(fixed_point_num << _fractional_bits)
 {
-
+    std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float fixed_point_num): _fixed_point_num(static_cast<int>(roundf(fixed_point_num * (1 << _fractional_bits))))
 {
-
+    std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& src)
 {
+    std::cout << "Copy constructor called" << std::endl;
     _fixed_point_num = src.getRawBits();
 }
 
 Fixed::~Fixed(void)
 {
-
+    std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(Fixed const& rhs)
 {
+    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &rhs)
     {
         this->_fixed_point_num = rhs.getRawBits();
