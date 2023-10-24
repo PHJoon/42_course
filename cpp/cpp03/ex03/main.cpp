d66b6430fb20450ccdfb2851b82a36bce15dce14
PHJoon <phj9503@gmail.com>
2023-10-24 14:24:30 +0900

    cpp03

--- cpp/cpp03/ex03/main.cpp
+++ cpp/cpp03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyungjpa <hyungjpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:44:36 by hyungjpa          #+#    #+#             */
-/*   Updated: 2023/10/24 14:07:41 by hyungjpa         ###   ########.fr       */
+/*   Updated: 2023/10/24 14:24:22 by hyungjpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,12 @@ int main(void)
     ClapTrap    t("Tom");
     ClapTrap    *pInterface = NULL;
     ClapTrap    *pBase = new ClapTrap("A");
-    ClapTrap    *pChildB = new FragTrap("B");
+    ClapTrap    *pChildB = new ScavTrap("B");
     ClapTrap    *pChildC = new FragTrap("C");
-    FragTrap    *pInterface2 = NULL;
+    ClapTrap    *pChildD = new DiamondTrap("D");
+    ScavTrap    *pInterfaceScav = NULL;
+    FragTrap    *pInterfaceFrag = NULL;
+    DiamondTrap    *pInterfaceDiamond = NULL;
     
     std::cout << std::endl;
     std::cout << "--- ClapTrap A ---" << std::endl << std::endl;
@@ -38,34 +41,63 @@ int main(void)
     t.takeDamage(pInterface->getAD());
     std::cout << std::endl;
 
-    std::cout << "--- Frag Trap B ---" << std::endl << std::endl;
+    std::cout << "--- Scav Trap B ---" << std::endl << std::endl;
     pInterface = pChildB;
     pInterface->attack(t.getName());
     t.takeDamage(pInterface->getAD());
+    std::cout << std::endl;
+    
+    std::cout << "--- ScavTrap B info ---" << std::endl;
+    std::cout << pInterface->getName() << "/" << pInterface->getHP() << "/" << pInterface->getEP() << "/" << pInterface->getAD() << std::endl;
     std::cout << std::endl;
 
     std::cout << "--- Frag Trap C ---" << std::endl << std::endl;
     pInterface = pChildC;
     pInterface->attack(t.getName());
     t.takeDamage(pInterface->getAD());
+
+    std::cout << "--- FragTrap C info ---" << std::endl;
+    std::cout << pInterface->getName() << "/" << pInterface->getHP() << "/" << pInterface->getEP() << "/" << pInterface->getAD() << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "--- Diamond Trap C ---" << std::endl << std::endl;
+    pInterface = pChildD;
+    pInterface->attack(t.getName());
+    t.takeDamage(pInterface->getAD());
     std::cout << std::endl;
 
+    std::cout << "--- DiamondTrap D info ---" << std::endl;
+    std::cout << pInterface->getName() << "/" << pInterface->getHP() << "/" << pInterface->getEP() << "/" << pInterface->getAD() << std::endl;
+    std::cout << std::endl;
 
     std::cout << "--- DownCasting ---"<< std::endl;
     // error -> 다운캐스팅 필요
     // pInterface->guardGate();
-    pInterface2 = dynamic_cast<FragTrap *>(pChildC);
-    if (!pInterface2) { std::cout << "fail dynamic cast" << std::endl;}
-    pInterface2->highFivesGuys();
+
+    pInterfaceScav = dynamic_cast<ScavTrap *>(pChildB);
+    if (!pInterfaceScav) { std::cout << "fail dynamic cast" << std::endl;}
+    pInterfaceScav->guardGate();
+    
+    pInterfaceFrag = dynamic_cast<FragTrap *>(pChildC);
+    if (!pInterfaceFrag) { std::cout << "fail dynamic cast" << std::endl;}
+    pInterfaceFrag->highFivesGuys();
+    
+    pInterfaceDiamond = dynamic_cast<DiamondTrap *>(pChildD);
+    if (!pInterfaceFrag) { std::cout << "fail dynamic cast" << std::endl;}
+    pInterfaceDiamond->guardGate();
+    pInterfaceDiamond->highFivesGuys();
+    pInterfaceDiamond->whoAmI();
 
     std::cout << std::endl;
-    pInterface = pChildB;
-    pInterface->attack(pInterface2->getName());
-    pInterface2->takeDamage(pInterface->getAD());
-    pInterface->beRepaired(20);
+    
+    pInterface = pChildD;
+    pInterface->attack(pInterfaceDiamond->getName());
+    pInterfaceDiamond->takeDamage(pInterface->getAD());
+    pInterfaceDiamond->beRepaired(20);
 
     std::cout << std::endl;
     std::cout << "--- Destructor ---" << std::endl;
+    delete pChildD;
     delete pChildB;
     delete pChildC;
     delete pBase;
