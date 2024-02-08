601ac9d46197f701e315dfe8f4e8b8dc028a301e
PHJoon <phj9503@gmail.com>
2024-02-08 13:15:57 +0900

    mod ft_irc/setting.sh

--- ft_irc/setting.sh
+++ ft_irc/setting.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-apt-get update -y
-apt-get install -y irssi inspircd tcpflow
+sudo apt-get update -y
+sudo apt-get install -y irssi inspircd tcpflow
+
+sudo chown -R root:root /etc/inspircd/inspircd.conf
+sudo mkdir /var/run/inspircd
+sudo touch /var/run/inspircd/inspircd.pid
+sudo chmod 777 /var/run/inspircd/inspircd.pid
+
+# sudo inspircd --runasroot --nofork --dubug
+# sudo irssi -c 127.0.0.1 -p 6667 -n nickname
+# sudo tcpflow -i lo port 6667 -c
