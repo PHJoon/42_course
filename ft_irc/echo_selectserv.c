c8dce42c8bb00886b4321026b43056be61d395b7
PHJoon <phj9503@gmail.com>
2024-02-13 10:11:28 +0900

    mod ft_irc/echo_selectserv.c

--- ft_irc/echo_selectserv.c
+++ ft_irc/echo_selectserv.c
@@ -9,6 +9,34 @@
 
 #define BUF_SIZE 100
 
+#define WELCOME ":irc.local 001 hj :Welcome to the Localnet IRC Network hj!root@127.0.0.1\n\
+:irc.local 002 hj :Your host is irc.local, running version InspIRCd-3\n\
+:irc.local 003 hj :This server was created 00:34:38 Feb 13 2024\n\
+:irc.local 004 hj irc.local InspIRCd-3 iosw biklmnopstv :bklov\n\
+:irc.local 005 hj AWAYLEN=200 CASEMAPPING=rfc1459 CHANLIMIT=#:20 CHANMODES=b,k,l,imnpst CHANNELLEN=64 CHANTYPES=# ELIST=CMNTU HOSTLEN=64 KEYLEN=32 KICKLEN=255 LINELEN=512 MAXLIST=b:100 :are supported by this server\n\
+:irc.local 005 hj MAXTARGETS=20 MODES=20 NETWORK=Localnet NICKLEN=30 PREFIX=(ov)@+ SAFELIST STATUSMSG=@+ TOPICLEN=307 USERLEN=10 WHOX :are supported by this server\n\
+:irc.local 251 hj :There are 0 users and 0 invisible on 1 servers\n\
+:irc.local 253 hj 1 :unknown connections\n\
+:irc.local 254 hj 0 :channels formed\n\
+:irc.local 255 hj :I have 0 clients and 0 servers\n\
+:irc.local 265 hj :Current local users: 0  Max: 0\n\
+:irc.local 266 hj :Current global users: 0  Max: 0\n\
+:irc.local 375 hj :irc.local message of the day\n\
+:irc.local 372 hj :- **************************************************\n\
+:irc.local 372 hj :- *             H    E    L    L    O              *\n\
+:irc.local 372 hj :- *  This is a private irc server. Please contact  *\n\
+:irc.local 372 hj :- *  the admin of the server for any questions or  *\n\
+:irc.local 372 hj :- *  issues.                                       *\n\
+:irc.local 372 hj :- **************************************************\n\
+:irc.local 372 hj :- *  The software was provided as a package of     *\n\
+:irc.local 372 hj :- *  Debian GNU/Linux <https://www.debian.org/>.   *\n\
+:irc.local 372 hj :- *  However, Debian has no control over this      *\n\
+:irc.local 372 hj :- *  server.                                       *\n\
+:irc.local 372 hj :- **************************************************\n\
+:irc.local 372 hj :- (The sysadmin possibly wants to edit </etc/inspircd/inspircd.motd>)\n\
+:irc.local 376 hj :End of message of the day."
+
+
 void error_handling(char *message) {
     fputs(message, stderr);
     fputs("\n", stderr);
@@ -48,6 +76,8 @@ int main(int ac, char **av) {
     FD_SET(serv_sock, &reads);
     fd_max = serv_sock;
 
+    int flag = 0;
+
     while (1) {
         cpy_reads = reads;
         timeout.tv_sec = 5;
@@ -78,6 +108,11 @@ int main(int ac, char **av) {
                         close(i);
                         printf("closed client: %d\n", i);
                     } else {
+                        write(1, buf, str_len);
+                        if (!flag) {
+                            write(i, WELCOME, strlen(WELCOME));
+                            flag = 1;
+                        }
                         write(i, buf, str_len);
                     }
                 }
